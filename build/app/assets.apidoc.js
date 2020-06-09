9a0451b016a4afe3b9279a0485d7dfaf48d931d7
Travis CI User <travis@example.org>
2020-06-09 14:18:24 +0000

    [ci skip] update gh-pages

--- build/app/assets.apidoc.js
+++ build/app/assets.apidoc.js
@@ -13,12 +13,10 @@
 /* jslint utility2:true */
 /* istanbul ignore next */
 // run shared js-env code - init-local
-(function (globalThis) {
+(function () {
     "use strict";
     let consoleError;
     let local;
-    // init globalThis
-    globalThis.globalThis = globalThis.globalThis || globalThis;
     // init debugInline
     if (!globalThis.debugInline) {
         consoleError = console.error;
@@ -169,7 +167,7 @@
             throw err;
         });
     }
-}((typeof globalThis === "object" && globalThis) || window));
+}());
 // assets.utility2.header.js - end
 
 
